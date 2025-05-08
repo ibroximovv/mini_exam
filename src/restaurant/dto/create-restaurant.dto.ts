074f9ef9ae062d0c3a3037233f2f96cc4991a724
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-05-08 16:53:16 +0500

    restaurant

--- src/restaurant/dto/create-restaurant.dto.ts
+++ src/restaurant/dto/create-restaurant.dto.ts
@@ -1,8 +1,31 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsBoolean, IsNumber, IsPhoneNumber, IsString, IsUUID } from "class-validator";
+import { UUID } from "crypto";
 
 export class CreateRestaurantDto {
     @ApiProperty({ example: 'Nt' })
     @IsString()
     name: string
+
+    @ApiProperty({ example: 'Chilonzor' })
+    @IsString()
+    adress: string
+
+    @ApiProperty({ example: '+998910128133' })
+    @IsPhoneNumber()
+    phone: string
+
+    @ApiProperty({ example: 15 })
+    @Type(() => Number)
+    @IsNumber()
+    tip: number
+
+    @ApiProperty()
+    @IsUUID()
+    regionId: UUID
+
+    @ApiProperty({ example: true })
+    @IsBoolean()
+    isActive: boolean
 }
--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -1,26 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Region } from 'src/region/entities/region.entity';
 
 @Injectable()
 export class RestaurantService {
-  create(createRestaurantDto: CreateRestaurantDto) {
-    return 'This action adds a new restaurant';
+  constructor(private readonly prisma: PrismaService){}
+  async create(createRestaurantDto: CreateRestaurantDto) {
+    try {
+      const findregion = await this.prisma.region.findFirst({ where: { id: createRestaurantDto.regionId }})
+      if (!findregion) throw new BadRequestException('Region not found.')
+      const findone = await this.prisma.restaurant.findFirst({
+        where: {
+          AND: [
+            { name: createRestaurantDto.name },
+            { adress: createRestaurantDto.adress },
+            { phone: createRestaurantDto.phone }, // isActive ham qoshiladi sal keyinroq
+          ]
+        }
+      })
+      if (findone) throw new BadRequestException('Restaurant already exists.')
+      return await this.prisma.restaurant.create({ data: createRestaurantDto });
+    } catch (error) {
+      if(error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(error.message || 'Internal server Error')
+    }
   }
 
-  findAll() {
-    return `This action returns all restaurant`;
+  async findAll() {
+    try {
+      return await this.prisma.restaurant.findMany({
+        include: {
+          regions: true
+        },
+        omit: {
+          regionId: true
+        }
+      });
+    } catch (error) {
+      if(error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(error.message || 'Internal server Error')
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} restaurant`;
+  async findOne(id: string) {
+    try {
+      const findone = await this.prisma.restaurant.findFirst({ where: { id }})
+      if (!findone) throw new BadRequestException('Restaurant not found')
+      return findone;
+    } catch (error) {
+      if(error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(error.message || 'Internal server Error')
+    }
   }
 
-  update(id: number, updateRestaurantDto: UpdateRestaurantDto) {
-    return `This action updates a #${id} restaurant`;
+  async update(id: string, updateRestaurantDto: UpdateRestaurantDto) {
+    try {
+      const findone = await this.prisma.restaurant.findFirst({ where: { id }})
+      if (!findone) throw new BadRequestException('Restaurant not found')
+      const findregion = await this.prisma.region.findFirst({ where: { id: updateRestaurantDto.regionId }})
+      if (!findregion) throw new BadRequestException('Region not found.')
+        const findOne = await this.prisma.restaurant.findFirst({
+          where: {
+            AND: [
+              { name: updateRestaurantDto.name },
+              { adress: updateRestaurantDto.adress },
+              { phone: updateRestaurantDto.phone }, // isActive ham qoshiladi sal keyinroq
+            ]
+          }
+        })
+      if (findOne) throw new BadRequestException('Restaurant already exists.')
+      return await this.prisma.restaurant.update({ where: { id }, data: updateRestaurantDto });
+    } catch (error) {
+      if(error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(error.message || 'Internal server Error')
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} restaurant`;
+  async remove(id: string) {
+    try {
+      const findone = await this.prisma.restaurant.findFirst({ where: { id }})
+      if (!findone) throw new BadRequestException('Restaurant not found')
+      return await this.prisma.restaurant.delete({ where: { id }});
+    } catch (error) {
+      if(error instanceof BadRequestException) throw error;
+      throw new InternalServerErrorException(error.message || 'Internal server Error')
+    }
   }
 }
