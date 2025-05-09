a9381288665d712630b1a36920999646c847743c
Lenovo <islomtoxirov08@gmail.com>
2025-05-09 14:19:37 +0500

    rbac

--- src/debt/debt.controller.ts
+++ src/debt/debt.controller.ts
@@ -1,59 +1,76 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { DebtService } from './debt.service';
 import { CreateDebtDto } from './dto/create-debt.dto';
 import { UpdateDebtDto } from './dto/update-debt.dto';
 import { ApiQuery } from '@nestjs/swagger';
+import { Roles } from 'src/common/role.decorator';
+import { UserRole } from '@prisma/client';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
 
 @Controller('debt')
 export class DebtController {
   constructor(private readonly debtService: DebtService) {}
 
+  @Roles(UserRole.CASHER, UserRole.OWNER)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Post()
   create(@Body() createDebtDto: CreateDebtDto) {
     return this.debtService.create(createDebtDto);
   }
 
   @ApiQuery({
-    name: "sort",
+    name: 'sort',
     required: false,
-    enum: ["amount", "client"]
+    enum: ['amount', 'client'],
   })
   @ApiQuery({
-    name: "order",
+    name: 'order',
     required: false,
-    enum: ["asc", "desc"]
+    enum: ['asc', 'desc'],
   })
   @ApiQuery({
-    name: "from",
-    required: false
+    name: 'from',
+    required: false,
   })
   @ApiQuery({
-    name: "take",
-    required: false
+    name: 'take',
+    required: false,
   })
   @ApiQuery({
-    name: "client",
-    required: false
+    name: 'client',
+    required: false,
   })
   @ApiQuery({
-    name: "restaurantId",
-    required: false
+    name: 'restaurantId',
+    required: false,
   })
   @ApiQuery({
-    name: "orderId",
-    required: false
+    name: 'orderId',
+    required: false,
   })
   @Get()
   findAll(
-    @Query("orderId") orderId: number,
-    @Query("restaurantId") restaurantId:  string,
-    @Query("client") client: string,
-    @Query("sort") sort: string,
-    @Query("order") order: string,
-    @Query("take") take: string,
-    @Query("from") from: string
+    @Query('orderId') orderId: number,
+    @Query('restaurantId') restaurantId: string,
+    @Query('client') client: string,
+    @Query('sort') sort: string,
+    @Query('order') order: string,
+    @Query('take') take: string,
+    @Query('from') from: string,
   ) {
-    let options = {orderId, restaurantId,client, sort, order, take, from}
+    let options = { orderId, restaurantId, client, sort, order, take, from };
     return this.debtService.findAll(options);
   }
 
@@ -62,11 +79,18 @@ export class DebtController {
     return this.debtService.findOne(id);
   }
 
+  @Roles(UserRole.ADMIN, UserRole.CASHER, UserRole.OWNER, UserRole.SUPER_ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDebtDto: UpdateDebtDto) {
     return this.debtService.update(id, updateDebtDto);
   }
 
+
+  @Roles(UserRole.ADMIN, UserRole.CASHER, UserRole.OWNER, UserRole.SUPER_ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.debtService.remove(id);
--- src/withdraw/withdraw.controller.ts
+++ src/withdraw/withdraw.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { WithdrawService } from './withdraw.service';
 import { CreateWithdrawDto } from './dto/create-withdraw.dto';
 import { UpdateWithdrawDto } from './dto/update-withdraw.dto';
 import { ApiQuery } from '@nestjs/swagger';
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { UserRole } from '@prisma/client';
+import { Roles } from 'src/common/role.decorator';
 
 @Controller('withdraw')
 export class WithdrawController {
   constructor(private readonly withdrawService: WithdrawService) {}
 
+  @Roles(UserRole.CASHER, UserRole.ADMIN, UserRole.OWNER)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Post()
   create(@Body() createWithdrawDto: CreateWithdrawDto) {
     return this.withdrawService.create(createWithdrawDto);
@@ -50,16 +67,25 @@ export class WithdrawController {
   })
   @Get()
   findAll(
-    @Query("price") price: number,
-    @Query("type") type: string,
-    @Query("orderId") orderId: number,
-    @Query("restaurantId") restaurantId: number,
-    @Query("take") take: number,
-    @Query("from") from: number,
-    @Query("order") order: number,
-    @Query("sort") sort: number,
+    @Query('price') price: number,
+    @Query('type') type: string,
+    @Query('orderId') orderId: number,
+    @Query('restaurantId') restaurantId: number,
+    @Query('take') take: number,
+    @Query('from') from: number,
+    @Query('order') order: number,
+    @Query('sort') sort: number,
   ) {
-    let options = {price, type, orderId, restaurantId, take, from, order, sort}
+    let options = {
+      price,
+      type,
+      orderId,
+      restaurantId,
+      take,
+      from,
+      order,
+      sort,
+    };
     return this.withdrawService.findAll(options);
   }
 
@@ -68,6 +94,9 @@ export class WithdrawController {
     return this.withdrawService.findOne(id);
   }
 
+  @Roles(UserRole.CASHER, UserRole.ADMIN, UserRole.OWNER)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -76,6 +105,9 @@ export class WithdrawController {
     return this.withdrawService.update(id, updateWithdrawDto);
   }
 
+  @Roles(UserRole.CASHER, UserRole.ADMIN, UserRole.OWNER)
+  @UseGuards(RolesGuard)
+  @UseGuards(AuthorizationGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.withdrawService.remove(id);
