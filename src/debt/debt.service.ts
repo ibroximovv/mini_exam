d29683e8cf4367b251b1dc7870b417d6e06e4b71
Lenovo <islomtoxirov08@gmail.com>
2025-05-09 14:08:36 +0500

    try catch

--- src/debt/debt.service.ts
+++ src/debt/debt.service.ts
@@ -8,74 +8,97 @@ export class DebtService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreateDebtDto) {
-    let db = await this.prisma.debt.findFirst({
-      where: { client: data.client },
-    });
-    if (db) {
-      throw new BadRequestException(
-        `Client "${data.client}" already have a debt`,
-      );
+    try {
+      let db = await this.prisma.debt.findFirst({
+        where: { client: data.client },
+      });
+      if (db) {
+        throw new BadRequestException(
+          `Client "${data.client}" already have a debt`,
+        );
+      }
+      let created = await this.prisma.debt.create({ data });
+      return { created };
+    } catch (error) {
+      return error.message
     }
-    let created = await this.prisma.debt.create({ data });
-    return { created };
   }
 
   async findAll(options) {
-    let where: any = {};
+    try {
+      let where: any = {};
 
-    if (options.orderId) where.orderId = options.orderId;
-    if (options.client) where.client = options.client;
-    if (options.restaurantId) where.restaurantId = options.restaurantId;
+      if (options.orderId) where.orderId = options.orderId;
+      if (options.client) where.client = options.client;
+      if (options.restaurantId) where.restaurantId = options.restaurantId;
 
-    const take = options.take ? parseInt(options.take) : undefined;
-    const skip = options.from ? parseInt(options.from) : undefined;
+      const take = options.take ? parseInt(options.take) : undefined;
+      const skip = options.from ? parseInt(options.from) : undefined;
 
-    const orderBy = options.sort
-      ? {
-          [options.sort]: options.order === 'desc' ? 'desc' : 'asc',
-        }
-      : undefined;
+      const orderBy = options.sort
+        ? {
+            [options.sort]: options.order === 'desc' ? 'desc' : 'asc',
+          }
+        : undefined;
 
-    const datas = await this.prisma.debt.findMany({
-      where,
-      take,
-      skip,
-      orderBy,
-      include: {
-        orders: true,
-        restaurants: {
-          select: { name: true },
+      const datas = await this.prisma.debt.findMany({
+        where,
+        take,
+        skip,
+        orderBy,
+        include: {
+          orders: true,
+          restaurants: {
+            select: { name: true },
+          },
         },
-      },
-    });
+      });
 
-    return datas;
+      return datas;
+    } catch (error) {
+      console.log(error);
+      return error.message
+    }
   }
 
   async findOne(id: string) {
-    let one = await this.prisma.debt.findFirst({ where: { id } });
-    if (!one) {
-      throw new NotFoundException('Not Found');
-    }
+    try {
+      let one = await this.prisma.debt.findFirst({ where: { id } });
+      if (!one) {
+        throw new NotFoundException('Not Found');
+      }
 
-    return { one };
+      return { one };
+    } catch (error) {
+      console.log(error);
+      return error.message
+      
+    }
   }
 
   async update(id: string, data: UpdateDebtDto) {
-    let one = await this.prisma.debt.findFirst({ where: { id } });
-    if (!one) {
-      throw new NotFoundException('Not Found');
+    try {
+      let one = await this.prisma.debt.findFirst({ where: { id } });
+      if (!one) {
+        throw new NotFoundException('Not Found');
+      }
+      let updated = await this.prisma.debt.update({ where: { id }, data });
+      return updated;
+    } catch (error) {
+      return error.message
     }
-    let updated = await this.prisma.debt.update({ where: { id }, data });
-    return updated;
   }
 
   async remove(id: string) {
-    let one = await this.prisma.debt.findFirst({ where: { id } });
-    if (!one) {
-      throw new NotFoundException('Not Found');
+    try {
+      let one = await this.prisma.debt.findFirst({ where: { id } });
+      if (!one) {
+        throw new NotFoundException('Not Found');
+      }
+      let deleted = await this.prisma.debt.delete({ where: { id } });
+      return deleted;
+    } catch (error) {
+      return error.message
     }
-    let deleted = await this.prisma.debt.delete({ where: { id } });
-    return deleted;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,61 +10,87 @@ export class UserService {
   constructor(private readonly prisma: PrismaService){}
 
   async create(data: CreateUserDto) {
-    let user = await this.prisma.user.findFirst({ where: { phone: data.phone } })
-    if (user) {
-      if (user.role === data.role) {
-        throw new BadRequestException(`${data.role} with this phone already exists`)
+    try {
+      let user = await this.prisma.user.findFirst({
+        where: { phone: data.phone },
+      });
+      if (user) {
+        if (user.role === data.role) {
+          throw new BadRequestException(
+            `${data.role} with this phone already exists`,
+          );
+        }
       }
+      let hash = bcrypt.hashSync(data.password, 10);
+      let newUser = await this.prisma.user.create({
+        data: { ...data, password: hash },
+      });
+      return newUser;
+    } catch (error) {
+      return error.message
     }
-    let hash = bcrypt.hashSync(data.password, 10)
-    let newUser = await this.prisma.user.create({data: {...data, password: hash}})
-    return newUser
   }
 
   async findAll(options) {
-    let where: any = {}
-    if(options.name)where.name = options.name
-    if(options.role)where.role = options.role
-    if(options.restaurantId)where.restaurantId = options.restaurantId
-    if (options.phone) where.phone = options.phone
-    
-    let orderBy: { [key: string]: 'asc' | 'desc' }[] | undefined = undefined;
+    try {
+      let where: any = {};
+      if (options.name) where.name = options.name;
+      if (options.role) where.role = options.role;
+      if (options.restaurantId) where.restaurantId = options.restaurantId;
+      if (options.phone) where.phone = options.phone;
 
-    if (options.sort) {
-      const order: 'asc' | 'desc' = options.order === 'desc' ? 'desc' : 'asc';
-      orderBy = [{ [options.sort]: order }];
+      let orderBy: { [key: string]: 'asc' | 'desc' }[] | undefined = undefined;
+
+      if (options.sort) {
+        const order: 'asc' | 'desc' = options.order === 'desc' ? 'desc' : 'asc';
+        orderBy = [{ [options.sort]: order }];
+      }
+
+      const skip = options.from ? Number(options.from) : 0;
+      const take = options.take ? Number(options.take) : 10;
+
+      let users = await this.prisma.user.findMany({ where });
+      return users;
+    } catch (error) {
+      return error.message
     }
-
-    const skip = options.from ? Number(options.from) : 0;
-    const take = options.take ? Number(options.take) : 10;
-
-    let users = await this.prisma.user.findMany({where})
-    return users
   }
 
   async findOne(id: string) {
-    let one = await this.prisma.user.findFirst({where: {id}})
-    if (!one) {
-      throw new NotFoundException(`User with id(${id}) not found`)
+    try {
+      let one = await this.prisma.user.findFirst({ where: { id } });
+      if (!one) {
+        throw new NotFoundException(`User with id(${id}) not found`);
+      }
+      return one;
+    } catch (error) {
+      return error.message
     }
-    return one
   }
 
   async update(id: string, data: UpdateUserDto) {
-    let user = this.findOne(id)
-    if (!user) {
-      throw new NotFoundException(`User with id(${id}) not found`);
+    try {
+      let user = this.findOne(id);
+      if (!user) {
+        throw new NotFoundException(`User with id(${id}) not found`);
+      }
+      let updatedUser = await this.prisma.user.update({ where: { id }, data });
+      return updatedUser;
+    } catch (error) {
+      return error.message
     }
-    let updatedUser = await this.prisma.user.update({where: {id}, data})
-    return updatedUser
   }
 
   async remove(id: string) {
-    let user = this.findOne(id);
-    if (!user) {
-      throw new NotFoundException(`User with id(${id}) not found`);
+    try {
+      let user = this.findOne(id);
+      if (!user) {
+        throw new NotFoundException(`User with id(${id}) not found`);
+      }
+      let deleted = await this.prisma.user.delete({ where: { id } });
+      return deleted;
+    } catch (error) {
+      return error.message
     }
-    let deleted = await this.prisma.user.delete({where: {id}})
-    return deleted
   }
 }
